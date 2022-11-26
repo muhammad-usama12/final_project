13da2e9ff43e0231603049569ccd417cbf22c0cf
kethnal <kaseyvaldez@hotmail.com>
2022-11-25 17:11:24 -0800

    Break Article into smaller componens

--- frontend-react/src/components/Article.js
+++ frontend-react/src/components/Article.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./Article.scss"
 import Category from "./Category"
+import ArticleRightButtons from "./ArticleRightButtons";
 
 // We pass props from App.js
 // Will probably make another component that passes props to Article.js instead of App.js similar to DayList and DayListItem from scheduler
 export default function Article(props) {
   return (
     <article>
-
       <div className="screen-and-buttons">
-
         <div className="screen">
           <p>
             {props.text}
@@ -21,28 +20,9 @@ export default function Article(props) {
           >
           </img>
         </div>
-
-        <div className="article-buttons">
-          <img 
-          className="profile-icon"
-          src="https://i.pinimg.com/474x/ce/9c/ab/ce9cab218f2849c81f230e4296fd120c.jpg"
-          alt="profile"
-          >
-          </img>
-
-          <div className="actions">
-            <i className="fa-solid fa-star"></i>
-            <i className="fa-solid fa-circle-plus"></i>
-            <i className="fa-solid fa-comment-dots"></i>
-          </div>
-        </div>
-
+        <ArticleRightButtons />
       </div>
-
-      <Category
-        name={props.category}
-      />
-
+      <Category name={props.category} />
     </article>
   );
 }
---
+++ frontend-react/src/components/Comments.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Comments.scss"
+
+export default function Comments(props) {
+  return (
+    <div className="article-tags">
+      {props.name}
+    </div>
+  );
+}
