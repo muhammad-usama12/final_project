7b753dce2c8bb1b7fff902345d75a12c1395b37c
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-26 19:17:42 -0500

    create login, signup and textfield component

---
+++ frontend-react/src/components/Registration/SignUp.jsx
@@ -0,0 +1,85 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Button, ButtonGroup, Heading, Text, VStack } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import TextField from "./TextField";
+
+const SignUp = () => {
+  // const { setUser } = useContext(AccountContext);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={(values, actions) => {
+        const vals = { ...values };
+        actions.resetForm();
+        fetch("/auth/signup", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(vals),
+        })
+          .catch((err) => {
+            return;
+          })
+          .then((res) => {
+            if (!res || !res.ok || res.status >= 400) {
+              return;
+            }
+            return res.json();
+          })
+          .then((data) => {
+            if (!data) return;
+            if (data.status) {
+              setError(data.status);
+            } else if (data.loggedIn) {
+              navigate("/home");
+            }
+          });
+      }}
+    >
+      <VStack
+        as={Form}
+        w={{ base: "90%", md: "500px" }}
+        m="auto"
+        justify="center"
+        h="100vh"
+        spacing="1rem"
+      >
+        <Heading>Sign Up</Heading>
+        <Text as="p" color="red.500">
+          {error}
+        </Text>
+        <TextField
+          name="username"
+          placeholder="Enter username"
+          autoComplete="off"
+          label="Username"
+        />
+
+        <TextField
+          name="password"
+          placeholder="Enter password"
+          autoComplete="off"
+          label="Password"
+          type="password"
+        />
+
+        <ButtonGroup pt="1rem">
+          <Button colorScheme="teal" type="submit">
+            Create Account
+          </Button>
+          <Button onClick={() => navigate("/")} leftIcon={<ArrowBackIcon />}>
+            Back
+          </Button>
+        </ButtonGroup>
+      </VStack>
+    </Formik>
+  );
+};
+
+export default SignUp;
