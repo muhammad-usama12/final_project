fdc9d4ddd33e642ec14deb01568125f003ff2357
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-27 15:11:13 -0500

    deleted an import

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { ColorModeScript } from "@chakra-ui/color-mode";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import theme from "./theme";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
