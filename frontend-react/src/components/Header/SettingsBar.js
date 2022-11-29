f678e06a047197f23ad20dcd8d143e79724ffa79
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-29 17:32:54 -0500

    fix formatting

--- frontend-react/src/components/Header/SettingsBar.js
+++ frontend-react/src/components/Header/SettingsBar.js
@@ -2,12 +2,9 @@ import React from "react";
 import "./Header.scss";
 
 export default function SettingsBar(props) {
-
   return (
     <section className="settings">
-      <div className="pill-container">
-        Edit Profile
-      </div>
+      <div className="pill-container">Edit Profile</div>
       <div className="pill-container" onClick={props.onLogOut}>
         Logout
       </div>
