5f31348ae993f9caa38fc3e08dfe2ccfd67651f2
Devi Raju <deviraju999@gmail.com>
2022-11-29 12:08:47 -0800

    add header

--- frontend-react/src/components/Registration/Login.jsx
+++ frontend-react/src/components/Registration/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
             if (data.status) {
               setError(data.status);
             } else if (data.loggedIn) {
-              navigate("/home");
+              navigate("/");
             }
           });
       }}
--- frontend-react/src/components/views.jsx
+++ frontend-react/src/components/views.jsx
@@ -2,17 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Registration/Login";
 import SignUp from "./Registration/SignUp";
 import Profile from "./Profile";
-import App from "./App";
+
 
 const Views = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/home/*" element={<App />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
-
 export default Views;
