b5695b9eed53c65b0fd64a41208b3e4e68a67ad1
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-29 20:11:54 -0500

    add a redirect button to test PrivateRoutes

--- frontend-react/src/components/profile/index.js
+++ frontend-react/src/components/profile/index.js
@@ -1,21 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Profile.scss";
 
 export default function Profile(props) {
   return (
     <section className="profile-header">
-      <img 
+      <img
         className="profile-display-picture"
         src="https://i.pinimg.com/474x/ce/9c/ab/ce9cab218f2849c81f230e4296fd120c.jpg"
         alt="profile"
-      >
-      </img>
+      ></img>
       <div className="handle-and-bio">
         <div className="handle">
           <h1>@blackbeardsbarANDmilfs</h1>
         </div>
         <div className="bio">
-          <p>adam / 27 / 🇨🇦  / the office is my personality yes you can bully me but occasionally i have opinions on friends</p>
+          <p>
+            adam / 27 / 🇨🇦 / the office is my personality yes you can bully me
+            but occasionally i have opinions on friends
+          </p>
+          <Link to="/dash">
+            <div className="pill-container">DASHBOARD</div>
+          </Link>
         </div>
       </div>
     </section>
