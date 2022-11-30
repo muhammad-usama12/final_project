62bcbe3d5695559c7409cec70a2bbfb647db61bf
kethnal <kaseyvaldez@hotmail.com>
2022-11-30 12:07:57 -0800

    Make Profile dynamically render user attributes

--- frontend-react/src/components/profile/index.js
+++ frontend-react/src/components/profile/index.js
@@ -1,33 +1,69 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Header from "../Header";
+
 import "./Profile.scss";
 
+import CategoryList from "../CategoryList";
+import Header from "../Header";
+import Article from "../Article";
+
+import useApplicationData from "../../hooks/useApplicationData";
+import { getCurrentUser, getPostsByUser, getShowForPost } from "../../helpers/selectors";
+import { AccountContext } from "../AccountContext";
+
 export default function Profile(props) {
+  const {
+    state,
+    hideSpoiler,
+    handleSpoilerToggle,
+    getFilteredShows,
+    getAllShows,
+  } = useApplicationData()
+
+  const { user } = useContext(AccountContext);
+
+  const currentUser = getCurrentUser(state, user.userId);
+  const posts = getPostsByUser(state, user.userId)
+
+  const articleList = posts.map((post) => {
+  const show = getShowForPost(state, post.tvshow_id);
+
+  return (
+    <Article
+      key={post.id}
+      {...post}
+      show={show}
+      user={currentUser}
+      spoiler={hideSpoiler && post.spoiler}
+    />
+  );
+});
+
   return (
     <>
       <Header />
       <section className="profile-header">
         <img
           className="profile-display-picture"
-          src="https://i.pinimg.com/474x/ce/9c/ab/ce9cab218f2849c81f230e4296fd120c.jpg"
+          src={currentUser.icon_url}
           alt="profile"
         ></img>
         <div className="handle-and-bio">
           <div className="handle">
-            <h1>@blackbeardsbarANDmilfs</h1>
+            <h1>@{currentUser.username}</h1>
           </div>
           <div className="bio">
             <p>
-              adam / 27 / 🇨🇦 / the office is my personality yes you can bully me
-              but occasionally i have opinions on friends
+              {currentUser.bio}
             </p>
             <Link to="/dash">
-              <div className="pill-container">DASHBOARD</div>
+              <div className="pill-container">Dashboard</div>
             </Link>
           </div>
         </div>
       </section>
+      {/* <CategoryList /> */}
+      <section className="article-container">{articleList}</section>
     </>
   );
 }
