b6541ffccf46263ac10044299a0995d43ac50a3c
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-29 20:07:58 -0500

    restructure the file to function with routes and Private Routes

--- frontend-react/src/components/App.js
+++ frontend-react/src/components/App.js
@@ -13,52 +13,9 @@ import useApplicationData from "../hooks/useApplicationData";
 import { getShowForPost, getUserForPost } from "../helpers/selectors";
 
 function App() {
-  const {
-    state,
-    hideSpoiler,
-    handleSpoilerToggle,
-    getFilteredShows,
-    getAllShows,
-  } = useApplicationData();
-
-  const articleList = state.posts.map((post) => {
-    const show = getShowForPost(state, post.tvshow_id);
-    const user = getUserForPost(state, post.user_id);
-
-    return (
-      <Article
-        key={post.id}
-        {...post}
-        show={show}
-        user={user}
-        spoiler={hideSpoiler && post.spoiler}
-      />
-    );
-  });
-
-  console.log("cookie", document.cookie);
-
   return (
     <UserContext>
       <Views />
-      <div>
-        <Header />
-        <main>
-          {/* <EditProfile /> */}
-          {/* <Profile /> */}
-          <section className="category-filters">
-            <CategoryList
-              shows={state.shows}
-              hideSpoilers={handleSpoilerToggle}
-              getFilteredShows={getFilteredShows}
-              getAllShows={getAllShows}
-            />
-          </section>
-          {/* <button onClick={getCookie}>getCookie</button> */}
-          {document.cookie && <NewPost />}
-          <section className="article-container">{articleList}</section>
-        </main>
-      </div>
     </UserContext>
   );
 }
