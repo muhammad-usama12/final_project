79889bb5ce85580bb1ce97de32a34361f97ad5ba
kethnal <kaseyvaldez@hotmail.com>
2022-11-29 17:45:08 -0800

    Make header available on all views

--- frontend-react/src/components/views.jsx
+++ frontend-react/src/components/views.jsx
@@ -2,28 +2,31 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Registration/Login";
 import SignUp from "./Registration/SignUp";
 import Profile from "./Profile";
-import App from "./App";
 import PrivateRoutes from "./PrivateRoutes";
 import Compilation from "./Article/Compilation";
 import Guest from "./Article/Guest";
 import { useContext } from "react";
 import { AccountContext } from "./AccountContext";
+import Header from "./Header";
 
 const Views = () => {
   const { user } = useContext(AccountContext);
   return user.loggedIn === null ? (
     ""
   ) : (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/guest" element={<Guest />} />
-      <Route element={<PrivateRoutes />}>
-        <Route path="/dash" element={<Compilation />} />
-        <Route path="/profile" element={<Profile />} />
-      </Route>
-      <Route path="*" element={<Login />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/guest" element={<Guest />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/dash" element={<Compilation />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </>
   );
 };
 
