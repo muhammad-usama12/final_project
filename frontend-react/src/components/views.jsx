deac801c682a0f81987c49c5d8ba25e9bab2f5b5
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-12-01 12:57:14 -0500

    fix format

--- frontend-react/src/components/views.jsx
+++ frontend-react/src/components/views.jsx
@@ -7,16 +7,15 @@ import { useContext } from "react";
 import { AccountContext } from "./AccountContext";
 
 const Views = () => {
-
   const user = useContext(AccountContext);
-  console.log("user.loggedin &&&", user.user.loggedIn)
+  console.log("user.loggedin &&", user.user.loggedIn);
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<App />} />
     </Routes>
-  )
+  );
 };
 
 export default Views;
