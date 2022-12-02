82ab0266889a5d9cf57a8892b2bd5d8ceb42dbca
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-12-02 18:25:05 -0500

    add lightSwitch controller component

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -4,19 +4,26 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import Views from "./components/views";
 import UserContext from "./components/AccountContext";
+import theme from "./theme";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraComponent } from "@chakra-ui/react";
 
 import "./index.css";
 
 import Scripts from "./components/Scripts";
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Scripts />
-      <UserContext>
-        <Views />
-      </UserContext>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Scripts />
+        <UserContext>
+          <Views />
+        </UserContext>
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
