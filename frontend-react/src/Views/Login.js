d3a15f1478fd55555b5c855561ed4f3dbd41eb20
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-12-05 13:35:56 -0500

    implement chakra provider to login

--- frontend-react/src/Views/Login.js
+++ frontend-react/src/Views/Login.js
@@ -3,6 +3,8 @@ import { Button, ButtonGroup, Heading, Text, VStack } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { useNavigate } from "react-router";
 import TextField from "../components/TextField";
+import theme from "../components/theme";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -13,82 +15,85 @@ export const LoginContext = createContext();
 export default function Login() {
   const [error, setError] = useState();
   const [user, setUser] = useState({});
-  const state = useApplicationData()
+  const state = useApplicationData();
   const navigate = useNavigate();
   return (
     <>
       <Header />
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(values, actions) => {
-          const vals = { ...values };
-          actions.resetForm();
-          fetch("/api/auth/login", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(vals),
-          })
-            .catch((err) => {
-              return;
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          onSubmit={(values, actions) => {
+            const vals = { ...values };
+            actions.resetForm();
+            fetch("/api/auth/login", {
+              method: "POST",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(vals),
             })
-            .then((res) => {
-              if (!res || !res.ok) {
+              .catch((err) => {
                 return;
-              }
-              return res.json();
-            })
-            .then((data) => {
-              if (!data) return;
-              if (data.status) {
-                setError(data.status);
-              } else if (data.loggedIn) {
-                localStorage.setItem('teeboUser', data.userId);
-                setUser({ ...data });
-                state.setState((prev) => ({ ...prev, loggedIn: true }))
-                localStorage.setItem('teeboUser', data.userId);
-                navigate("/");
-              }
-            });
-        }}
-      >
-        <VStack
-          as={Form}
-          w={{ base: "90%", md: "500px" }}
-          m="auto"
-          justify="center"
-          h="100vh"
-          spacing="1rem"
+              })
+              .then((res) => {
+                if (!res || !res.ok) {
+                  return;
+                }
+                return res.json();
+              })
+              .then((data) => {
+                if (!data) return;
+                if (data.status) {
+                  setError(data.status);
+                } else if (data.loggedIn) {
+                  localStorage.setItem("teeboUser", data.userId);
+                  setUser({ ...data });
+                  state.setState((prev) => ({ ...prev, loggedIn: true }));
+                  localStorage.setItem("teeboUser", data.userId);
+                  navigate("/");
+                }
+              });
+          }}
         >
-          <Heading>Log In</Heading>
-          <Text as="p" color="red.500">
-            {error}
-          </Text>
-          <TextField
-            name="username"
-            placeholder="Enter username"
-            autoComplete="off"
-            label="Username"
-          />
+          <VStack
+            as={Form}
+            w={{ base: "90%", md: "500px" }}
+            m="auto"
+            justify="center"
+            h="100vh"
+            spacing="1rem"
+          >
+            <Heading>Log In</Heading>
+            <Text as="p" color="red.500">
+              {error}
+            </Text>
+            <TextField
+              name="username"
+              placeholder="Enter username"
+              autoComplete="off"
+              label="Username"
+            />
 
-          <TextField
-            name="password"
-            placeholder="Enter password"
-            autoComplete="off"
-            label="Password"
-            type="password"
-          />
+            <TextField
+              name="password"
+              placeholder="Enter password"
+              autoComplete="off"
+              label="Password"
+              type="password"
+            />
 
-          <ButtonGroup pt="1rem">
-            <Button colorScheme="teal" type="submit">
-              Log In
-            </Button>
-          </ButtonGroup>
-        </VStack>
-      </Formik>
-      <Footer />
+            <ButtonGroup pt="1rem">
+              <Button colorScheme="yellow" type="submit">
+                Log In
+              </Button>
+            </ButtonGroup>
+          </VStack>
+        </Formik>
+        <Footer />
+      </ChakraProvider>
     </>
   );
-};
+}
