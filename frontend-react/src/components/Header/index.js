b12970548127653c85fef7ed3630710439ec46b0
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-12-05 14:21:06 -0500

    add to header design

--- frontend-react/src/components/Header/index.js
+++ frontend-react/src/components/Header/index.js
@@ -8,30 +8,32 @@ export default function Header(props) {
   //   "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTE5IDZoLTQuNTlsMi4zLTIuMjlhMSAxIDAgMSAwLTEuNDItMS40MkwxMiA1LjU5bC0zLjI5LTMuM2ExIDEgMCAxIDAtMS40MiAxLjQyTDkuNTkgNkg1YTMgMyAwIDAgMC0zIDN2MTBhMyAzIDAgMCAwIDMgM2gxNGEzIDMgMCAwIDAgMy0zVjlhMyAzIDAgMCAwLTMtM1ptLTMgMTBhMiAyIDAgMCAxLTIgMkg2YTIgMiAwIDAgMS0yLTJ2LTZhMiAyIDAgMCAxIDItMmg4YTIgMiAwIDAgMSAyIDJabTMgM2ExIDEgMCAxIDEgMS0xIDEgMSAwIDAgMS0xIDFabTAtNGExIDEgMCAxIDEgMS0xIDEgMSAwIDAgMS0xIDFabTAtNGExIDEgMCAxIDEgMS0xIDEgMSAwIDAgMS0xIDFaIiBmaWxsPSIjZjZmMWYxIiBjbGFzcz0iZmlsbC00NjQ2NDYiPjwvcGF0aD48L3N2Zz4=";
 
   return (
-    <div className="App">
-      <nav className="Navbar">
-        <Link to="/">
-          <h2>teebo</h2>
-        </Link>
-        <ul>
-          <li>{!document.cookie && <Link to="/signup">Sign Up</Link>}</li>
-          <li>{!document.cookie && <Link to="/login">Log In</Link>}</li>
-          <li>
-            {document.cookie && (
-              <Link to="/profile" onClick={props.profile}>
-                Profile
-              </Link>
-            )}
-          </li>
-          <li>
-            {document.cookie && (
-              <Link to="#" onClick={props.logout}>
-                Logout
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <header>
+      <div className="App">
+        <nav className="Navbar">
+          <Link to="/">
+            <h2>teebo</h2>
+          </Link>
+          <ul>
+            <li>{!document.cookie && <Link to="/signup">Sign Up</Link>}</li>
+            <li>{!document.cookie && <Link to="/login">Log In</Link>}</li>
+            <li>
+              {document.cookie && (
+                <Link to="/profile" onClick={props.profile}>
+                  Profile
+                </Link>
+              )}
+            </li>
+            <li>
+              {document.cookie && (
+                <Link to="#" onClick={props.logout}>
+                  Logout
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
