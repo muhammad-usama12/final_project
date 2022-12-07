d798d7886ac1f72f6f6695abb3b354fbc920f4b9
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-12-07 17:22:34 -0500

    create toggle button for theme transitions

---
+++ frontend-react/src/components/Toggle.js
@@ -0,0 +1,24 @@
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const Toggle = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Button
+      onClick={() => toggleColorMode()}
+      pos="absolute"
+      top="0"
+      right="0"
+      m="2.5rem"
+    >
+      {colorMode === "dark" ? (
+        <SunIcon color="orange.200" />
+      ) : (
+        <MoonIcon color="blue.700" />
+      )}
+    </Button>
+  );
+};
+
+export default Toggle;
